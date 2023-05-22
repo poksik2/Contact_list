ed728d3e229f0712b5bd8ecd0e45196cdc4070f9
poksik2 <poksik322@gmail.com>
2023-05-22 17:54:14 +0300

    0.2

--- 3.py
+++ 3.py
@@ -1,8 +1,10 @@
 import json
-from typing import List, Any
 
 
 class book:
+    def __init__(self) -> None:
+        self.list = []
+        self.keylist = {}
 
     def main(self):
         while True:
@@ -16,9 +18,6 @@ class book:
                 break
 
 
-    def __init__(self) -> None:
-        self.list = []
-        self.keylist = {}
 
 
 
@@ -29,30 +28,30 @@ class book:
         number = input()
 
         self.keylist[name] = number
-        self.list = json.dumps(self.keylist)
+        self.list = json.dumps(self.keylist, sort_keys=True)
         #self.list = json.loads(self.list)
-
-
 
 
         imports = open('3.txt', 'wb')
         imports.write(self.list.encode())
+        self.list.encode()
         imports.close()
 
 
-        #print(type(self.list))
+
+
+        print(type(self.list))
         #print(type(self.keylist))
         #self.dell()
         #print(self.list)
 
 
     def exports(self) -> None:
-        exports = open('3.txt', 'rb')
-
-
-        self.list = exports.readline()
-
-        #print(type(self.list))
+        exports = open('3.txt', 'r')
+        self.list = exports.read()
+        self.list = self.list.encode()
+        self.list = json.load(self.list.decode())
+        print(type(self.list))
         print(self.list)
         exports.close()
 
