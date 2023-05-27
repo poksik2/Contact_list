783b9e79aebe5fe3facc0697de0452113db58f47
nookie <nookie.totopor@yandex.ru>
2023-05-27 21:44:38 +0300

    review

--- Contact_book.py
+++ Contact_book.py
@@ -1,45 +1,78 @@
 class BookMessage:
+    MENU = 'Что будем делать? (Посмотреть - 1, Добавить или редактировать - 2, Удалить - 3,  Выход - Any Key): '
+    TITLE = '====Список контактов===='
+    SUCCESS_CREATED = 1
+    # ПРОДОЛЖИ
+
     def get_menu_msg(self) -> str:
         return 'Что будем делать? (Посмотреть - 1, Добавить или редактировать - 2, Удалить - 3,  Выход - Any Key): '
-    def tile_string(self)-> str:
+
+    @staticmethod
+    def tile_string() -> str:
         return '====Список контактов===='
-    def enter_name_msg(self)-> str:
+
+    def enter_name_msg(self) -> str:
         return 'Введите имя контакта: '
-    def enter_number_msg(self)-> str:
+
+    def enter_number_msg(self) -> str:
         return 'Ведите номер контакта: '
+
     def enter_new_name_msg(self):
         return 'Ведите новый номер контакта: '
+
     def number_len_err(self):
         return f'Длинна номера должна быть {BookList.NUMBER_LEN}'
-    def empty_name_msg(self)-> str:
+
+    def empty_name_msg(self) -> str:
         return 'Имя не может быть пустым'
-    def overwrite_name_or_no(self, name)-> str:
+
+    def overwrite_name_or_no(self, name) -> str:
+        # добавить новый или изменить старый
         return f'Контакт с таким именем уже есть. Хотите изменить номер для {name} (Да - 1, Нет - 2): '
-    def get_exit_msg(self)-> str:
+
+    def get_exit_msg(self) -> str:
         return 'ВСЕГО ХОРОШЕГО!!!'
-    def enter_name_for_dellete(self)-> str:
+
+    def enter_name_for_dellete(self) -> str:
         return 'Введите имя контакта который хотите удалить: '
-    def delete_contact_true(self)-> str:
+
+    def delete_contact_true(self) -> str:
         return 'Контакт удален!'
+
     def added_successfully_contact(self) -> str:
         return 'Контакт успешно добавлен!'
-    def file_if_creating(self)-> str:
+
+    def file_if_creating(self) -> str:
+        # naming
+        # file_was_created
         return 'Файл создан'
 
+
 class BookList:
+    # добавить эти функции
+    # create_name
+    # create_number
+    # naming:
+    # метод должен содержать глагол в начальной форме (create, read, write, validate)
+    # и не быть слишком абстрактным (не содержать общие слова/фразы типа: информация,
+    # данные, список, коллекция),а быть конкретным (контакты, фио)
     NUMBER_LEN = 5
+
     def __init__(self):
-        self.contact_list = {}
+        # реализовать классы для чтения, добавления, удаления и тд контактов
+        # self.creator = 1
+        # self.reader|getter = 1
+        self.contact_list = {}  # naming
         self.msg_creator = BookMessage()
 
     def main(self):
         try:
             self.start_program()
         finally:
-            self.complite()
+            self.complete_program()
 
     def start_program(self):
-
+        # пофиксить повторный вызов этого метода
         while True:
             self.initialize_contact()
             choice = self.suggest_are_choice()
@@ -53,51 +86,65 @@ class BookList:
                 self.write_list_to_file()
                 self.complite()
 
-    def complite(self) -> None:
+    def complete_program(self) -> None:
         print(self.msg_creator.get_exit_msg())
         exit()
 
     def initialize_contact(self):
+        # !!!!
         try:
             self.import_to_list()
         except:
             self.create_file()
 
     def import_to_list(self):
+        # название файла в переменную
         with open('my_contact_list.txt', 'r', encoding='utf-8') as file:
             file_string = file.readlines()
             for string in file_string:
                 string = string.strip()
                 name, number = string.split(', ')
+                # write_contact
                 self.contact_list[name] = number
 
     def create_file(self):
         with open('my_contact_list.txt', 'w', encoding='utf-8') as file:
             return print(self.msg_creator.file_if_creating)
+
     def suggest_are_choice(self) -> str:
+        # naming
+        # create_menu
         return input(self.msg_creator.get_menu_msg())
 
     def read_the_list(self):
+        # naming
         print(self.msg_creator.tile_string())
         for key, value in self.contact_list.items():
             print(key, value)
 
     def write_contact(self):
+        # naming
         self.receiving_and_verifying_and_write_data()
+
     def receiving_and_verifying_and_write_data(self):
-        name = self.entering_name()
-        phone_number = self.entering_number()
+        # naming
+        name = input(self.msg_creator.enter_name_msg())
+        self.validation_name(name)
+        phone_number = input(self.msg_creator.enter_number_msg())
+        self.validation_number(phone_number)
         self.write_data_to_list(name, phone_number)
 
     def entering_name(self):
-        name = self.input_name()
+        name = 'name'
         self.validation_name(name)
         return name
 
     def input_name(self):
         name = input(self.msg_creator.enter_name_msg())
         return name
+
     def validation_name(self, name: str):
+        # naming
         if not name:
             self.action_empty_name()
         else:
@@ -116,9 +163,10 @@ class BookList:
         if choice_action_name == '1':
             new_number = input(self.msg_creator.enter_new_name_msg())
             self.validation_number(new_number)
+            # нужен метод для редактирования
             self.contact_list[name] = new_number
-            self.write_list_to_file()
-            self.start_program()
+            self.write_list_to_file()  # !!!!
+            self.start_program()  # !!!!
 
         elif choice_action_name == '2':
             self.write_contact()
@@ -137,11 +185,12 @@ class BookList:
             print(self.msg_creator.added_successfully_contact())
         else:
             print(self.msg_creator.number_len_err())
+            # запустить ввод номера повторно
             self.start_program()
 
     def write_data_to_list(self, name, phone_number):
         self.contact_list[name] = phone_number
-        self.start_program()
+        self.start_program()  # !!!!
 
     def delete_contact(self):
         name = self.input_name_for_delete()
@@ -150,13 +199,14 @@ class BookList:
         print(self.msg_creator.delete_contact_true())
 
     def input_name_for_delete(self):
-        name = input(self.msg_creator.enter_name_for_dellete())
-        return name
+        return input(self.msg_creator.enter_name_for_dellete())
 
     def write_list_to_file(self):
         with open('my_contact_list.txt', 'w', encoding='utf-8') as file:
             for name, number in self.contact_list.items():
                 file.write(f'{name}, {number}\n')
+                file.writelines(f'{name}, {number}')  # если работает это, то выбери его
+
 
 start = BookList()
 start.main()
