13e72303f18df385628843beca95bc6385c6e582
poksik2 <poksik322@gmail.com>
2023-06-02 16:46:52 +0300

    Книга контактов по файлам 02.06.23

---
+++ parser.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+from urllib.request import urlopen
+
+class Parser:
+    URL_SITE = "https://pogoda.mail.ru/prognoz/bryansk/"
+
+    def pars(self):
+        r = requests.get(self.URL_SITE)
+        print(r)
+        bs = BeautifulSoup(r.text, "html.parser")
+        print(bs)
+        table = bs.find('div', class_="information__content__temperature")
+
+        print(table.text)
+
+
+p = Parser()
+p.pars()
