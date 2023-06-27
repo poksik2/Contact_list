b7a7b18297fd0acb64c4fec3a6a834ad2a0640e6
poksik2 <poksik322@gmail.com>
2023-06-27 17:54:31 +0300

    Parser 27.06.23

--- book_write_contacts.py
+++ book_write_contacts.py
@@ -6,13 +6,11 @@ class BookWriterContacts:
 
     def save_contacts(self, contacts):
         with open(self.FILE_NAME, 'w', encoding='utf-8') as file:
-            i = 0
             for name, number in contacts.items():
                 number = str(number)
                 number = number.strip('[]')
-                print(type(number),number)
+                number = number.replace('\'', '')
                 file.write(f'{name}: {number}\n')
-                i+=1
 
     def validate_contacts_for_saving(self):
         pass
